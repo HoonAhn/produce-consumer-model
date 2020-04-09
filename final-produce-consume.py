a6394959ae6a9489ef910d761a9ed7ad7d4be8cf
Hoon Ahn <shahn801@gmail.com>
2020-04-09 14:15:16 +0900

    Final implementation with python Queue.\ Python Queue well optimized for multi-threads. Next step is implementing the thread-safety queue manually with Condition variable.

--- final-produce-consume.py
+++ final-produce-consume.py
@@ -3,31 +3,35 @@ from queue import Queue
 import time
 import random
 
-condition = Condition()
-MAX_BUFFER = 100
-id = 0
-list_queue = []
+MAX_BUFFER = 5000
+
+# condition = Condition()
+# list_queue = []
 q = Queue(MAX_BUFFER)
 
 class ProducerThread(Thread):
+    def __init__(self, id):
+        Thread.__init__(self)
+        self.id = id
     def run(self):
-        global list_queue, i
-        
         while True:
             # condition.acquire()
             # if len(list_queue) == MAX_BUFFER:
                 # print("Buffer full")
                 # condition.wait()
+            
             data = self.create_data()
-            print("Produced: ", data)
-            # list_queue.append(data)
             q.put(data)
+            print("PRThread[%d] Produced[%d]: " % (self.id,data), list(q.queue))
+            
+            # list_queue.append(data)
             # condition.notify()
             # condition.release()
-            time.sleep(random.randint(1,10))
-            # print(list_queue)
-            print(q.queue)
+            
+            # (3) Producer will need certain time(1~15sec) to produce the resource.
+            time.sleep(random.randint(1,15))
     
+    # (2) Produced data is random integer between 1~99.
     def create_data(self):
         return random.randint(1,99)
 
@@ -37,30 +41,50 @@ class ConsumerThread(Thread):
         self.id = id
 
     def run(self):
-        global list_queue, i
         while True:
+
+            # (5)
             # condition.acquire()
             # if len(list_queue) < 1:
             #     print("Thread[%d]: Buffer Empty" % self.id)
             #     condition.wait()
             #     print("Thread[%d]: Woke up" % self.id)
             # data = list_queue.pop(0)
+            
+            # (4)
             data = q.get()
-            print("Thread[%d] Consumed: " % self.id, data)
+            print("CSThread[%d] Consumed[%d]: " % (self.id, data), list(q.queue))
+
             # condition.notify()
             # condition.release()
+
+            # I assumed that consumer need 3sec to use the resource.
             time.sleep(3)
 
 pr_threads = []
-
-for i in range(5):
-    pr_threads.append(ProducerThread())
-for th in pr_threads:
+cs_threads = []
+threads = []
+# Five Producer
+for i in range(1,6):
+    # pr_threads.append(ProducerThread())
+    threads.append(ProducerThread(i))
+for i in range(1,3):
+    threads.append(ConsumerThread(i))
+# for th in pr_threads:
+#     th.start()
+# for th in pr_threads:
+#     th.join()
+for th in threads:
     th.start()
+for th in threads:
+    th.join()
 
-con_th_1 = ConsumerThread(1)
-con_th_2 = ConsumerThread(2)
-con_th_1.start()
-con_th_2.start()
+# Two Consumer
+# for i in range(1,3):
+#     cs_threads.append(ConsumerThread(i))
+# for th in cs_threads:
+#     th.start()
+# for th in cs_threads:
+#     th.join()
 
 print("<End>")
